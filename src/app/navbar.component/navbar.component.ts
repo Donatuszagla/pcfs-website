6e89f2c07b6f593c24e734d6c7038f263c6f6361
Donatus Zagla <beneesolomon765@gmail.com>
2025-12-03 14:43:29 +0000

    minor home updates

--- src/app/navbar.component/navbar.component.ts
+++ src/app/navbar.component/navbar.component.ts
@@ -13,9 +13,18 @@ import { Router, RouterLink } from '@angular/router';
 export class NavbarComponent {
   readonly title = signal('Paradise City of Faith Sanctuary');
 
-  private router = inject(Router)
-  navigateToAbout() { 
+  private router = inject(Router);
+
+  navigateToAbout() {
     this.router.navigate(['/about']);
   }
-
+  navigateToBranches() {
+    this.router.navigate(['/branches']);
+  }
+  navigateToMedia() {
+    this.router.navigate(['/media']);
+  }
+  navigateToEvents() {
+    this.router.navigate(['/events']);
+  }
 }
